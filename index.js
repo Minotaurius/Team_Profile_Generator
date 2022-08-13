e7d963ee2b41362f2e8ae5fcddf899a5da8aaf7e
Minotaurius <brandonslanger@gmail.com>
2022-08-13 18:28:35 -0500

    Added html and css to place cards in

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const inquirer = require('./node_modules/inquirer')
-const fs = require('fs')
+// packages
+const inquirer = require('./node_modules/inquirer');
+const fs = require('fs');
+
+// importing classes
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// place to store anyone we register in our prompts
+const workers = []
