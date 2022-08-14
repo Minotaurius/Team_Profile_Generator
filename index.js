9799e62a8e51ce6533c17e52ae159027b70c6f2b
Minotaurius <brandonslanger@gmail.com>
2022-08-13 19:18:49 -0500

    Added manager questions, correctly prompting user and saving answers

--- index.js
+++ index.js
@@ -9,4 +9,38 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 // place to store anyone we register in our prompts
-const workers = []
+const teamArr = [];
+
+// place to store our questions we will ask user in terminal
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "Please enter your name so we can start building the rest of your team"
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Please enter your employee ID"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter your email so the team knows how to contact you"
+    },
+    {
+        type: 'input',
+        name: 'officeNum',
+        message: "Please enter your office number so the team knows where they can find you"
+    },
+]
+// manager prompts
+
+function addManager() {
+    inquirer.prompt(managerQuestions)
+    .then(answers => {
+        console.log(answers)
+    })
+};
+
+addManager()
